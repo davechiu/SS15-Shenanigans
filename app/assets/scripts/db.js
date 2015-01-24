4cc8bb13f9f572068fe4c660f9a152fbb5830927
Brandon Waring <brandon.waring@starcomww.com>
2015-01-23 20:31:03 -0500

    adding db js

---
+++ app/assets/scripts/db.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var APP = window.APP = window.APP || {};
+
+APP.global = (function(){
+
+    var initGlobalComponents = function() {
+        // APP.navigation.init();
+        // APP.footer.init();
+        APP.video.init();
+    };
+
+    var initPageComponents = function() {
+        APP.core.controller.init();
+    };
+
+    var init = function() {
+
+        var defaultVideoId = 'yt_M7lc1UVf-VE';
+        var videoId = (window.getURLParameter('videoId')) ? window.getURLParameter('videoId') : defaultVideoId;
+
+        APP.video.setVideoId(videoId);
+        /**
+        * initialize global components
+        */
+        initGlobalComponents();
+
+        /**
+        * initialize components for the current page
+        */
+        initPageComponents();
+    };
+
+    /**
+    * interfaces to public functions
+    */
+    return {
+        init: init
+    };
+
+}());
+
+$( document ).ready( APP.global.init );
