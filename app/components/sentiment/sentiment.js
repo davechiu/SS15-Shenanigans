3fd2f04f8ae1de60dbbc99d5e3fb454bae87689b
Kyle Bradshaw <kyle.bradshaw@digitas.com>
2015-01-24 13:31:14 -0500

    measuring sentiment

--- app/components/sentiment/sentiment.js
+++ app/components/sentiment/sentiment.js
@@ -13,6 +13,12 @@ APP.sentiment = (function(){
         return newDataObj;
     };
 
+    var getIntervalObj = function(interval, vote) {
+        var dataObj = {};
+        dataObj[interval] = vote;
+        return dataObj;
+    };
+
     //
     // template
     var voteObj = {
@@ -23,7 +29,7 @@ APP.sentiment = (function(){
 
     //triggered from $.subscribe vote event
     var postVote = function(event, value) {
-        // console.log(event, value);
+        console.log(event, value);
         //SEND THIS VOTE OFF TO FIREBASE
         $.unsubscribe('/video/currentTime', postVote);
     };
@@ -46,6 +52,7 @@ APP.sentiment = (function(){
     return {
         init: init,
         getNewDataObj: getNewDataObj,
+        getIntervalObj: getIntervalObj,
         postVote: postVote
     };
 
