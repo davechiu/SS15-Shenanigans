891931d3f631e3c2d16532b38eb67cc81a01834e
Brandon Waring <brandon.waring@starcomww.com>
2015-01-24 20:30:35 -0500

    wip

--- app/components/comments/comments.js
+++ app/components/comments/comments.js
@@ -77,6 +77,18 @@ APP.comments = (function(){
                     }
                 });
             });
+            watchFeedForUpdates();
+        });
+    };
+
+    var watchFeedForUpdates = function() {
+        var commentFeedRef = new Firebase(APP.db.getFbBase() + '/comments/'+videoId);
+        // 1. watch for new comments
+        commentFeedRef.on('child_added', function(childSnapshot, prevChildName) {
+            // code to handle new child.
+            console.log('child has been added');
+            //console.log('prev: '+prevChildName.val());
+            //console.log('new: '+childSnapshot.val());
         });
     };
 
